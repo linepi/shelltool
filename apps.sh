f79af3e6dc79363de9d29d0ab39fb15691ef4389
linepi <2428513224@qq.com>
2024-04-03 08:30:06 +0800

    fix some problem

--- apps.sh
+++ apps.sh
@@ -1,24 +1,25 @@
 __SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 __command_exists() {
-	if which $1 &> /dev/null; then
+	if hash $1 2>/dev/null; then
 		echo true
 	else
 		echo false
 	fi	
 }
 
-if [[ $(__command_exists fzf) == "false" ]]; then
+if ! [[ -f ~/.fzf.bash ]]; then
 	git clone --depth 1 https://github.com/junegunn/fzf.git ${__SCRIPT_DIR}/.fzf
 	yes | ${__SCRIPT_DIR}/.fzf/install
 	[ -f ~/.fzf.bash ] && source ~/.fzf.bash
+	echo "[ -f ~/.fzf.bash ] && source ~/.fzf.bash" >> ~/.bashrc
 fi
 
-if [[ $(__command_exists autojump) == "false" ]]; then
+if ! [[ -s ~/.autojump/etc/profile.d/autojump.sh ]]; then
 	git clone git://github.com/wting/autojump.git ${__SCRIPT_DIR}/.autojump
 	cd ${__SCRIPT_DIR}/.autojump
 	./install.py
 	cd -
-	[[ -s /home/wu/.autojump/etc/profile.d/autojump.sh ]] && source /home/wu/.autojump/etc/profile.d/autojump.sh
+	[[ -s ~/.autojump/etc/profile.d/autojump.sh ]] && source /home/wu/.autojump/etc/profile.d/autojump.sh
 	echo "[[ -s /home/wu/.autojump/etc/profile.d/autojump.sh ]] && source /home/wu/.autojump/etc/profile.d/autojump.sh" >> ~/.bashrc
 fi
