abd3ea396eb9553dfcd68bcf782cc379c906980d
linepi <2428513224@qq.com>
2024-05-03 20:09:38 +0800

    update centos action

--- preset.sh
+++ preset.sh
@@ -1,6 +1,7 @@
 if [[ "$HOME" != "/root" ]]; then
     alias sudo='sudo '
     alias apt='sudo apt'
+    alias yum='sudo yum'
     alias docker='sudo docker'
 else
     alias sudo=''
@@ -35,19 +36,27 @@ osinstall tmux
 cp $__SHELLTOOL_DIR/.tmux.conf ~/.tmux.conf
 
 if ! command_exists nvim; then
-    if ! command_exists add-apt-repository; then
-        osinstall software-properties-common
+    if command_exists apt; then
+        if ! command_exists add-apt-repository; then
+            osinstall software-properties-common
+        fi
+        yes | sudo add-apt-repository ppa:neovim-ppa/unstable
+        yes | sudo apt update
+        osinstall ripgrep
+    elif command_exists yum; then
+        osinstall yum-utils
+        sudo yum-config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/carlwgeorge/ripgrep/repo/epel-7/carlwgeorge-ripgrep-epel-7.repo
+        osinstall ripgrep
     fi
     if ! command_exists gcc; then
         osinstall build-essential
     fi
+
     osinstall unzip
-    yes | sudo add-apt-repository ppa:neovim-ppa/unstable
-    yes | sudo apt update
     osinstall neovim
-    osinstall ripgrep
     mkdir -p ~/.config
     cp -r $__SHELLTOOL_DIR/nvim ~/.config
+else
+    alias vim='nvim'
+    alias vi='nvim'
 fi
-alias vim='nvim'
-alias vi='nvim'
